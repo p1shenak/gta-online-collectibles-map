17ee847309a787fed6ca443c936da5b358f54f73
p1shenak <viktornikonov789@gmail.com>
2026-08-02 22:13:58 +0300

    script.js

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
+// Размер изображения карты
+const imageWidth = 8192;
+const imageHeight = 8192;
+
 // Создаём карту
 const map = L.map("map", {
     crs: L.CRS.Simple,
     minZoom: -2,
-    maxZoom: 4,
-    zoomControl: true
+    maxZoom: 3,
+    zoomControl: true,
+    attributionControl: false
 });
 
-// Пока используем простое изображение-заглушку
+// Границы изображения
 const bounds = [
     [0, 0],
-    [5000, 5000]
+    [imageHeight, imageWidth]
 ];
 
-const image = L.imageOverlay(
-    "https://placehold.co/5000x5000/2c2c2c/ffffff?text=GTA+Online+Map",
-    bounds
-);
+// Подключаем карту
+L.imageOverlay("map/map.jpg", bounds).addTo(map);
 
-image.addTo(map);
-
+// Подгоняем карту под экран
 map.fitBounds(bounds);
+
+// Ограничиваем перемещение
+map.setMaxBounds(bounds);
+
+// Пример маркера
+const marker = L.marker([4000, 4000]).addTo(map);
+
+marker.bindPopup(`
+<h3>Action Figure #1</h3>
+
+<button onclick="alert('Отмечено')">
+✔ Отметить найденной
+</button>
+
+<br><br>
+
+<button onclick="window.open('https://www.youtube.com/watch?v=SXe9X4rv9tc&t=1245s')">
+🎥 Посмотреть видео
+</button>
+`);
